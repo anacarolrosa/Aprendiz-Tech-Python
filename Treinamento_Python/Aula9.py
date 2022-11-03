3da01324fa924fd87c3305bbb78530c1f8546b49
ana.rosa <ana.rosa@pgmais.com.br>
2022-11-03 14:28:04 -0300

    123

---
+++ Treinamento_Python/Aula9.py
@@ -0,0 +1,14 @@
+# dim = float(input('Informe quantos vc tem na carteira: '))
+# dolar = float(input('Informe a cotação do dolar: '))
+# valor = dim/dolar 
+# print(f'Seu real deu para comprar {valor} de dolares')
+
+# dim = float(input('Informe quantos vc tem na carteira: '))
+# dolar = 5.40
+# valor = dim/dolar
+# n = "{:.2f}".format(valor)
+# print(n)
+
+dim = float(input('Informe quantos vc tem na carteira: '))
+dolar = 5.40
+print("{:.2f}".format(dim/dolar))
